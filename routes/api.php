5f89454a7ea0f3566b7a2ba04814caf9258f5637
Andjelija Milutinovic <milutinovici321@gmail.com>
2021-06-04 16:32:13 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\APIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//API
+Route::get('/products/get',[APIController::class,'getProducts']);
+Route::post('/orders/order',[APIController::class,'makeOrder']);
+Route::get('/orders/check/{id}',[APIController::class,'checkOrder']);
+Route::delete('/orders/cancel/{id}',[APIController::class,'cancelOrder']);
+Route::get('/orders/get',[APIController::class,'getOrders']);
+Route::get('/orders/total',[APIController::class,'totalOrderValue']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function (){
+    Route::get('/', [OrderController::class, 'index'])->name('home');
+    Route::get('/users',[UserController::class,'index'])->name('users');
+    Route::get('/categories',[CategoryController::class,'index'])->name('categoriesList');
+    Route::get('/categories/{category}',[CategoryController::class,'edit'])->name('categoriesEdit');
+    Route::delete('/categories/{category}',[CategoryController::class,'destroy'])->name('categoriesDestroy');
+    Route::get('/products',[ProductController::class,'index'])->name('productsList');
+    Route::get('/products/create',[ProductController::class,'create'])->name('productsCreate');
+    Route::get('/products/edit/{product}',[ProductController::class,'edit'])->name('productsEdit'); #za get prikazuje se productsedit 
+    Route::post('/products/create',[ProductController::class,'store'])->name('productsStore'); #za post prikazuje se productsStore fja 
+    Route::put('/products/edit/{product}',[ProductController::class,'update'])->name('productsUpdate');
+    Route::delete('/products/destroy/{product}',[ProductController::class,'destroy'])->name('productsDestroy');
 });
+
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
